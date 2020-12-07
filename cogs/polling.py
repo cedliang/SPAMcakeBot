426162059fc0cd04f81baaa98dc315f6413ff193
Cedric <clia0017@student.monash.edu>
2020-12-08 01:49:45 +1100

    refactored existing commands into cogs

--- cogs/polling.py
+++ cogs/polling.py
@@ -4,3 +4,8 @@ from discord.ext import commands
 class Polling(commands.Cog):
     def __init__(self, client):
         self.client = client
+
+    #@commands.command()
+
+def setup(client):
+    client.add_cog(Polling(client))
--- main.py
+++ main.py
@@ -7,26 +7,7 @@ client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
-    print('Bot is ready.')
-
-
-#returns the client ping
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'{round(client.latency*1000)}ms')
-
-
-####MODERATION
-
-#purges the last x messages, 5 by default
-@client.command()
-async def purge(ctx, amount = 5):
-    await ctx.channel.purge(limit = amount + 1)
-    if amount == 1:
-        pluralstring = 'message was'
-    else:
-        pluralstring = 'messages were'
-    await ctx.send(f'{amount} {pluralstring} purged.')
+    print('Bot initialised.')
 
 
 #COGS SETUP
