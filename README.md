bd3aafedc53d13229c8ceb3dce9d076f12e7b07c
Cedric <clia0017@student.monash.edu>
2020-12-08 00:04:14 +1100

    Added config.py file and gitignore

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SPAMcakeBot
 Discord bot for the server of the Monash Society of Physics, Astronomy and Maths
+
+
+Create a config.py file, with the token as a string variable.
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hi')
+import discord
+from discord.ext import commands
+import config
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+
+
+client.run(config.token)
