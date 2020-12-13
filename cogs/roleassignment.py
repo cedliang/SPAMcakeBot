d7a63078eb21581c76c2a1e667226462c1ed07f0
Cedric <clia0017@student.monash.edu>
2020-12-13 22:57:26 +1100

    added setup to roleassignment

---
+++ cogs/roleassignment.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+
+class RoleAssignment(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def purge(self, ctx, amount = 5):
+        await ctx.channel.purge(limit = amount + 1)
+        if amount == 1:
+            pluralstring = 'message was'
+        else:
+            pluralstring = 'messages were'
+        await ctx.send(f'{amount} {pluralstring} purged.')
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f'{round(self.client.latency*1000)}ms')
+
+
+def setup(client):
+    client.add_cog(RoleAssignment(client))
