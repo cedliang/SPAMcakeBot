5dcd92d5502ad9f766d63f8ac716ea3705c5408a
Ventus-cmd <wazif.m27@gmail.com>
2020-12-26 22:05:43 +1100

    Resolve merge conflict

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from discord.ext import commands
 import config
 import os
 
-client = commands.Bot(command_prefix = '.')
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix = '.', intents = intents)
 
 @client.event
 async def on_ready():
     print('Bot initialised.')
-
-
 
 #COGS SETUP
 @client.command()
