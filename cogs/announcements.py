7f144c5e4ebc5a06bd6853f4cabbb135763ab220
Ventus-cmd <wazif.m27@gmail.com>
2020-12-28 14:48:43 +1100

    Added show announcements, fixed bare try excepts

--- cogs/announcements.py
+++ cogs/announcements.py
@@ -16,6 +16,7 @@ class Announcements(commands.Cog):
         Format of inlineList = [[title, description], . . .]
         TODO:
             - Add textfile storage.
+            - Delete Announcements
         """
         self.client = client
         self.ann_list = []
@@ -47,20 +48,21 @@ class Announcements(commands.Cog):
 
             self.ann_list.append(ann_new)
             await ctx.send(f"Created Event - Title: {arg1} ID: {ann_id}")
-        except:
-            await ctx.send("Incorrect usage of command: !create_announcement [title]")
+        except IndexError:
+            await ctx.send("EventID not found.")
+
 
     @commands.command()
     @has_permissions(manage_roles=True)
-    async def mod_anntitle(self, ctx, arg1=None, arg2=None):
+    async def mod_anntitle(self, ctx, arg1=None):
         try:
             if arg1 is None:
                 arg1 = len(self.ann_list) - 1
             arg1 = int(arg1)
             title = await self.prompted_input(ctx, "Please enter the new title of the event: ")
             await self.assign_inarray(ctx, arg1, 1, title)
-        except:
-            await ctx.send("Incorrect usage of command: !mod_anntitle [eventID] [Title]")
+        except IndexError:
+            await ctx.send("EventID not found.")
 
     @commands.command()
     @has_permissions(manage_roles=True)
@@ -71,8 +73,8 @@ class Announcements(commands.Cog):
             arg1 = int(arg1)
             desc = await self.prompted_input(ctx, "Please enter the new description of the event: ")
             await self.assign_inarray(ctx, arg1, 2, desc)
-        except:
-            await ctx.send("Incorrect usage of command: !mod_anndesc [eventID] [description]")
+        except IndexError:
+            await ctx.send("EventID not found.")
 
     @commands.command()
     @has_permissions(manage_roles=True)
@@ -83,8 +85,8 @@ class Announcements(commands.Cog):
             arg1 = int(arg1)
             img_url = await self.prompted_input(ctx, "Please enter the new image url of the event: ")
             await self.assign_inarray(ctx, arg1, 3, img_url)
-        except:
-            await ctx.send("Incorrect usage of command: !mod_annimage [eventID] [image URL]")
+        except IndexError:
+            await ctx.send("EventID not found.")
 
     @commands.command()
     @has_permissions(manage_roles=True)
@@ -95,8 +97,8 @@ class Announcements(commands.Cog):
             arg1 = int(arg1)
             img_url = await self.prompted_input(ctx, "Please enter the new thumbnail url of the event: ")
             await self.assign_inarray(ctx, arg1, 4, img_url)
-        except:
-            await ctx.send("Incorrect usage of command: !mod_annthumbnail [eventID] [image URL]")
+        except IndexError:
+            await ctx.send("EventID not found.")
 
     @commands.command()
     @has_permissions(manage_roles=True)
@@ -120,79 +122,87 @@ class Announcements(commands.Cog):
             self.ann_list[arg1][6] = inline_appendage
         except IndexError:
             await ctx.send("Event ID does not exist.")
-        else:
-            await ctx.send("Incorrect usage of the command: !add_anninline [eventID]")
 
     @commands.command()
     @has_permissions(manage_roles=True)
     async def add_anntarget(self, ctx, arg1=None, arg2="everyone"):
-        try:
-            if arg1 is None:
-                arg1 = len(self.ann_list) - 1
-            arg1 = int(arg1)
+        if arg1 is None:
+            arg1 = len(self.ann_list) - 1
+        arg1 = int(arg1)
 
-            target_role = discord.utils.get(ctx.guild.roles, name=str(arg2))
-            self.ann_list[arg1][7] = target_role
-        except:
-            await ctx.send("Incorrect usage of the command: !add_anntarget [eventID] [target role]")
+        target_role = discord.utils.get(ctx.guild.roles, name=str(arg2))
+        self.ann_list[arg1][7] = target_role
 
     @commands.command()
     @has_permissions(manage_roles=True)
-    async def add_channeltarget(self, ctx, arg1=None, arg2="announcements"):
+    async def add_channeltarget(self, ctx, arg1=None):
         try:
             if arg1 is None:
                 arg1 = len(self.ann_list) - 1
             arg1 = int(arg1)
+            target = self.prompted_input(ctx, "Input channel target without the hash (#).")
+            self.ann_list[arg1][5] = target
+        except IndexError:
+            await ctx.send("Incorrect usage of command: !add_channeltarget [EventID]")
 
-            self.ann_list[arg1][5] = arg2
-        except:
-            await ctx.send("Incorrect usage of the command: !add_anntarget [eventID] [target role]")
+    @commands.command()
+    @has_permissions(manage_roles=True)
+    async def sh_announcement(self, ctx):
+        ann_len = len(self.ann_list)
+        embed_list = discord.Embed()
+        embed_list.title = "All Created Events: "
+        for i in range(ann_len):
+            outstr = "Target Channel: " + str(self.ann_list[i][5]) + "\n Description: " + str(self.ann_list[i][2])
+            embed_list.add_field(name=str(self.ann_list[i][0]) + " - " + str(self.ann_list[i][1]), value=outstr)
+        await ctx.send(embed=embed_list)
 
     @commands.command()
     @has_permissions(manage_roles=True)
     async def submit_announcement(self, ctx, arg1=None, arg2=None):
         # Arg1=EventID
         # Arg2=Time?
+        try:
+            if arg1 is None:
+                arg1 = len(self.ann_list) - 1
+            # Scheduler
+            if arg2 is not None:
+                arg2 = arg2.split(':')
+                cur_time = datetime.datetime.now()
+                delta = datetime.timedelta(hours=int(arg2[0]), minutes=int(arg2[1]),
+                                           seconds=int(arg2[2])) - datetime.timedelta(hours=cur_time.hour,
+                                                                                      minutes=cur_time.minute,
+                                                                                      seconds=cur_time.second)
+                delta_seconds = delta.total_seconds()
 
-        if arg1 is None:
-            arg1 = len(self.ann_list) - 1
-        # Scheduler
-        if arg2 is not None:
-            arg2 = arg2.split(':')
-            cur_time = datetime.datetime.now()
-            delta = datetime.timedelta(hours=int(arg2[0]), minutes=int(arg2[1]),
-                                       seconds=int(arg2[2])) - datetime.timedelta(hours=cur_time.hour,
-                                                                                  minutes=cur_time.minute,
-                                                                                  seconds=cur_time.second)
-            delta_seconds = delta.total_seconds()
+            arg1 = int(arg1)
 
-        arg1 = int(arg1)
+            cur_pos = self.ann_list[arg1]
 
-        cur_pos = self.ann_list[arg1]
+            embed = discord.Embed()
 
-        embed = discord.Embed()
+            embed.title = str(cur_pos[1])
+            embed.description = str(cur_pos[2])
 
-        embed.title = str(cur_pos[1])
-        embed.description = str(cur_pos[2])
+            if cur_pos[3] is not None: embed.set_image(url=cur_pos[3])
+            if cur_pos[4] is not None: embed.set_thumbnail(url=cur_pos[4])
+            if cur_pos[7] is not None: await ctx.send(cur_pos[7].mention)
 
-        if cur_pos[3] is not None: embed.set_image(url=cur_pos[3])
-        if cur_pos[4] is not None: embed.set_thumbnail(url=cur_pos[4])
-        if cur_pos[7] is not None: await ctx.send(cur_pos[7].mention)
+            #
+            if cur_pos[6] is not None:
+                for field in cur_pos[6]:
+                    embed.add_field(name=field[0], value=field[1], inline=True)
 
-        #
-        if cur_pos[6] is not None:
-            for field in cur_pos[6]:
-                embed.add_field(name=field[0], value=field[1], inline=True)
+            # Sleep until announcement time.
+            if arg2 is not None: await asyncio.sleep(delta_seconds)
 
-        # Sleep until announcement time.
-        if arg2 is not None: await asyncio.sleep(delta_seconds)
-
-        # Out the embed
-        if cur_pos[5] is None:
-            await ctx.send(embed=embed)
-        else:
-            channel = discord.utils.get(ctx.guild.text_channels, name=cur_pos[5])
-            await channel.send(embed=embed)
+            # Out the embed
+            if cur_pos[5] is None:
+                await ctx.send(embed=embed)
+            else:
+                channel = discord.utils.get(ctx.guild.text_channels, name=cur_pos[5])
+                await channel.send(embed=embed)
+        except IndexError:
+            await ctx.send("Invalid EventID")
 
 
 def setup(client):
