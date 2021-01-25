0fced08f8def2921479eb9d529ba9836193e2406
Cedric <clia0017@student.monash.edu>
2021-01-25 23:45:25 +1100

    moved loremflattened, cleaned up code

--- cogs/neil.py
+++ cogs/neil.py
@@ -2,21 +2,18 @@ import discord
 import random, sys
 from discord.ext import commands
 
-
 class Neil(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.neildict = None
 
-    @commands.Cog.listener()
-    async def on_ready():
-        with open("loremflattened.txt", encoding='utf8') as f:
-            readfile = f.read()
-            self.neildict = makerule(data, 2)
-
     @commands.command()
     async def neil(self, ctx):
-        wisetweet = makestring(rule, random.randint(15,35))
+        if self.neildict is None:
+            with open("loremflattened.txt", encoding='utf8') as f:
+                readfile = f.read()
+                self.neildict = makerule(readfile, 2)
+        wisetweet = makestring(self.neildict, random.randint(15,35))
         await ctx.send(wisetweet)
 
 def makerule(data, context):
@@ -24,7 +21,6 @@ def makerule(data, context):
     rule = {}
     words = data.split(' ')
     index = context
-
     for word in words[index:]:
         key = ' '.join(words[index-context:index])
         if key in rule:
@@ -32,32 +28,23 @@ def makerule(data, context):
         else:
             rule[key] = [word]
         index += 1
-
     return rule
-
 
 def makestring(rule, length):
     '''Use a given rule to make a string.'''
-
     validstart = False
     capitalise = False
 
     while not validstart:
         oldwords = random.choice(list(rule.keys())).split(' ') #random starting words
-
-
         if oldwords[0] == '':
             pass
-
         else:
-
             if (ord(oldwords[0][0]) >= 65) and (ord(oldwords[0][0]) <= 90):
                 validstart = True
             elif ord(oldwords[0][0]) >= 97 and ord(oldwords[0][0]) <= 122:
                 validstart = True
                 capitalise = True
-
-
 
     string = ' '.join(oldwords) + ' '
 
@@ -66,11 +53,9 @@ def makestring(rule, length):
             key = ' '.join(oldwords)
             newword = random.choice(rule[key])
             string += newword + ' '
-
             for word in range(len(oldwords)):
                 oldwords[word] = oldwords[(word + 1) % len(oldwords)]
             oldwords[-1] = newword
-
         except KeyError:
             if capitalise:
                 string = string[0].upper() + string[1:]
@@ -79,24 +64,15 @@ def makestring(rule, length):
             if lastperiodindex == -1:
                 return string[0:len(string)-1] + "."
             string = string[:lastperiodindex+1]
-
-
             return string
+
     if capitalise:
         string = string[0].upper() + string[1:]
-
-    print(string + "\n")
-
     lastperiodindex = string.rfind(".")
     if lastperiodindex == -1:
         return string[0:len(string)-1] + "."
     string = string[:lastperiodindex+1]
-
-
     return string
-
-
-
 
 def setup(client):
     client.add_cog(Neil(client))
